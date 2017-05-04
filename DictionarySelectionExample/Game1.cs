0a4a493101d3c107bd119e8583261c225cd0bc2f
ITSPaul <powell.paul@itsligo.ie>
2017-05-04 09:12:27 +0100

    Content Loader example

---
+++ DictionarySelectionExample/Game1.cs
@@ -0,0 +1,112 @@
+﻿using Engine.Engines;
+using Loader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace DictionarySelectionExample
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        string[] names;
+        Dictionary<string, Texture2D> images = new Dictionary<string, Texture2D>();
+        Texture2D current;
+        Random rgen = new Random();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            new InputEngine(this);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            images = ContentLoader.ContentLoad<Texture2D>(Content, "Characters");
+            names = new string[images.Count];
+
+            int i = 0;
+            foreach (var item in images)
+            {
+                names[i++] = item.Key;
+            }
+            string choice = names[rgen.Next(0, names.Length)];
+            current = images[choice];
+            //images.TryGetValue(choice, out current);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if(InputEngine.IsKeyPressed(Keys.Enter))
+            {
+                string choice = names[rgen.Next(0, names.Length)];
+                current = images[choice];
+            }
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(current,
+                                new Rectangle(
+                                        GraphicsDevice.Viewport.Bounds.Center, 
+                                        new Point(64, 64)),Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
